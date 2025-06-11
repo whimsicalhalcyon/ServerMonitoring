de37ff8fc943b348412238021937ebdf09fef99e
anastasia <qewesb@yandex.ru>
2025-06-11 17:24:22 +0300

    редактирование

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(),  tailwindcss()],
-  base: '/ServerMonitoring/',
+  base: process.env.NODE_ENV === process.env.NODE_ENV === 'production'
+      ? '/ServerMonitoring/'
+      : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 });
+
