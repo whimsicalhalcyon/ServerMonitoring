bfc7e91fdaa9823851452d8f012456a54df530f8
anastasia <qewesb@yandex.ru>
2025-06-17 18:30:59 +0300

    изменение панели с ошибками

--- src/components/Mistakes/PageMistakes.vue
+++ src/components/Mistakes/PageMistakes.vue
@@ -9,7 +9,7 @@ import TableMistakes from "@/components/Mistakes/TableMistakes.vue";
 
 export default {
   components: {
-    UiSelect, MainButton, UiInput, MistakeCheckbox, TableMistakes, UiCheckbox: UiCheckboxInteraction,
+    UiSelect, MainButton, UiInput, MistakeCheckbox, TableMistakes, UiCheckboxInteraction,
   },
   props: {
     themeLight: {
@@ -48,17 +48,19 @@ export default {
     <div class="panel" v-if="openPanel" :style="themeStatus ? {background: themeLight.backgroundComponent}: {background: themeDark.backgroundComponent}">
       <div class="top">
         <ui-input :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark"></ui-input>
-        <main-button :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark" class="download">
-          <i class="fa-solid fa-file-excel"></i>
-        </main-button>
+        <main-button :themeStatus="themeStatus" :themeLight="themeLight"
+                     :themeDark="themeDark" style="width: 4%;"><i class="fa-solid fa-file-excel"></i></main-button>
+      </div>
+      <div class="line"
+           :style="themeStatus?{borderColor:themeLight.borderColor}:{borderColor:themeDark.borderColor}"></div>
+      <div class="left-checkbox">
+      <div class="bottom">
         <ui-select class="select" :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">
           <option disabled selected>Выбрать интервал</option>
           <option>5 минут</option>
           <option>10 минут</option>
           <option>15 минут</option>
         </ui-select>
-      </div>
-      <div class="bottom">
         <ui-select class="select" :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">
           <option disabled>Состояние</option>
           <option>Любое</option>
@@ -70,12 +72,16 @@ export default {
         <ui-input class="date" placeholder="дд.мм.гггг 00:00" :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark"></ui-input>
         <main-button class="btn" :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">Найти</main-button>
         <main-button class="btn btn-close" :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">Сбросить</main-button>
+
       </div>
-      <div class="bottom">
-        <mistake-checkbox :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">Критическая</mistake-checkbox>
-        <mistake-checkbox :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">Предупреждение</mistake-checkbox>
-        <mistake-checkbox :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">Средняя</mistake-checkbox>
-        <mistake-checkbox :themeStatus="themeStatus" :themeLight="themeLight" :themeDark="themeDark">Высокая</mistake-checkbox>
+
+        <div class="textLeft">
+          <p style="margin-bottom: 10px"
+             :style="themeStatus?{color:themeLight.textCheckbox}:{color:themeDark.textCheckbox}">Тип
+            ошибки:</p>
+        </div>
+        <ui-checkbox-interaction class="check" :themeStatus="themeStatus"
+                                 :themeLight="themeLight" :themeDark="themeDark"></ui-checkbox-interaction>
       </div>
     </div>
     <div class="table-mistakes">
@@ -118,6 +124,14 @@ export default {
 .main .panel .bottom {
   display: flex;
   gap: 1.1%
+}
+
+.line {
+  border-bottom: 1px solid;
+}
+
+.check {
+  gap: 2%;
 }
 
 .main .panel .bottom .date {
